92bc90aa306852855a5fe828fb076e07a48a0ee5
Eugene <eugene@taranov.me>
2025-04-03 23:22:23 +0200

    disable zsh run during installation

--- lib/configure_shell.sh
+++ lib/configure_shell.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install p10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
